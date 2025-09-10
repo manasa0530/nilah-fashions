0f2b8d4efccdc92d08a64ae6343d709ca15fb531
manasa0530 <manasakondabathini8@gmail.com>
2025-09-10 23:40:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+
+
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:1,
+//             nav:false
+//         },
+//         1000:{
+//             items:2,
+//             nav:true
+//         }
+//     }
+// })
+
+// $('#owl-carousel1').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:3,
+//             nav:false
+//         },
+//         1000:{
+//             items:5,
+//             nav:true,
+//             loop:false
+//         }
+//     }
+// })
+
+$('#carousel1').owlCarousel({
+  loop: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true
+    },
+    600: {
+      items: 1,
+      nav: false
+    },
+    1000: {
+      items: 2,
+      nav: true
+    }
+  }
+});
+$('#carousel2').owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true
+    },
+    600: {
+      items: 3,
+      nav: false
+    },
+    1000: {
+      items: 5,
+      nav: true,
+      loop: false
+    }
+  }
+});
